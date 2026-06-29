61e5f09929fb2939bdc05ad9ba4e0adc392c00e6
oliver-cameron <94218640+oliver-cameron@users.noreply.github.com>
2026-06-29 18:56:16 +1000

    removed github ssh

--- extras.sh
+++ extras.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 cargo install bob-nvim tree-sitter-cli 
 bob install stable
-git config --global url."git@github.com:".insteadOf "https://github.com/"
 git clone https://github.com/NvChad/starter ~/.config/nvim
 sed -i '/require("lazy").setup({/a \  git = { timeout = 300 },\n  concurrency = 1,' ~/.config/nvim/lua/config/lazy.lua
 nvim --headless "+Lazy! sync" +MasonInstallAll +TSInstallAll +qa
